b60a1fd01f8feae679bc6f562d36b18f8666d807
Jmeas <jellyes2@gmail.com>
2018-04-08 09:45:01 -0700

    Fix CSP for KaTeX

--- server/utils/csp.js
+++ server/utils/csp.js
@@ -3,7 +3,7 @@ module.exports = {
     defaultSrc: ["'self'", 'https://www.google-analytics.com'],
     styleSrc: [
       "'self'",
-      // Normalize
+      // KaTeX fonts
       'cdnjs.cloudflare.com',
       // Material Design Icon font
       'cdn.materialdesignicons.com',
@@ -15,7 +15,9 @@ module.exports = {
       // Material Design Icon font
       'cdn.materialdesignicons.com',
       // Google fonts
-      'fonts.gstatic.com'
+      'fonts.gstatic.com',
+      // KaTeX fonts
+      'cdnjs.cloudflare.com'
     ]
   }
 };
