24b37b3bd10d70b9358df1a8cd204aae7df4c7bd
Jmeas <jellyes2@gmail.com>
2018-04-10 22:11:05 -0700

    Import pwa styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './styles/pwa.css';
 import './styles/typography.css';
 import './styles/hero.css';
 import App from './app';
